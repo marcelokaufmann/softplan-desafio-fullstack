c237ed7c023189913a3a10e333eb4277e2daf8d5
marcelokaufmann <marcelo.kaufmann@gmail.com>
2021-01-30 14:02:31 -0300

    commit inicial

---
+++ softplan-desafio-fullstack-marcelo-kaufmann/src/main/java/br/com/softplan/softplandesafiofullstackmarcelokaufmann/SoftplanDesafioFullstackMarceloKaufmannApplication.java
@@ -0,0 +1,13 @@
+package br.com.softplan.softplandesafiofullstackmarcelokaufmann;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftplanDesafioFullstackMarceloKaufmannApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftplanDesafioFullstackMarceloKaufmannApplication.class, args);
+	}
+
+}
---
+++ softplan-desafio-fullstack-marcelo-kaufmann/src/test/java/br/com/softplan/softplandesafiofullstackmarcelokaufmann/SoftplanDesafioFullstackMarceloKaufmannApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.softplan.softplandesafiofullstackmarcelokaufmann;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftplanDesafioFullstackMarceloKaufmannApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
